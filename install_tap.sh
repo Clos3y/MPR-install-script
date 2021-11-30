c827c515cccfc3e9bf27f6cea50f01b52c21d5bc
Clos3y <sam.w.close@gmail.com>
2021-11-30 14:26:05 +0000

    Added installer options

--- install_tap.sh
+++ install_tap.sh
@@ -1,5 +1,20 @@
 #!/bin/dash
 
+
+if [ $(apt list --installed | grep python3-setuptools | wc -l) -lt 1 ]
+then
+echo "Warning! python3-setuptools not installed! Would you like to install now? (y/n)"
+read answer
+if [ "$answer" = "yes" ] || [ "$answer" = "y" ]; 
+then 
+sudo apt update 
+sudo apt install python3-setuptools
+else 
+echo "You answered no! Aborting!"
+exit 1
+fi
+else fi
+
 # Clone tap
 git clone https://mpr.hunterwittenborn.com/tap.git
 
--- installer.sh
+++ installer.sh
@@ -2,9 +2,18 @@
 
 if [ $(apt list --installed | grep build-essential | wc -l) -lt 1 ]
 then
-echo "Warning! build-essential not installed! Aborting!"
+echo "Warning! build-essential not installed! Would you like to install now? (y/n)"
+read answer
+if [ "$answer" = "yes" ] || [ "$answer" = "y" ]; 
+then 
+sudo apt update 
+sudo apt install build-essential
+else 
+echo "You answered no! Aborting!"
 exit 1
-else
+fi
+else fi
+
 
 echo "Installing makedeb stable"
 
@@ -16,7 +25,7 @@ echo "Would you prefer tap (1, default) or stoke (2) as your MPR helper?"
 
 read mpr_helper
 
-if [ $mpr_helper = 2 ] || [ $mpr_helper = "stoke" ];
+if  [ "$mpr_helper" = "2" ]||  [ "$mpr_helper" = "stoke"  ];
 then
 echo "stoke is presently broken in my testing: aborting!"
 
